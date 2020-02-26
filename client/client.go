76936cf7afc2bfc9bf6a06c7e6c75de6916c9485
HEBCKUU_KOT <kod-t@mail.ru>
2020-02-26 11:21:27 +0300

    Create client.go

---
+++ client/client.go
@@ -0,0 +1,30 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// Client holds all of the information required to connect to a server
+type Client struct {
+	hostname   string
+	port       int
+	authToken  string
+	httpClient *http.Client
+}
+
+// NewClient returns a new client configured to communicate on a server with the
+// given hostname and port and to send an Authorization Header with the value of
+// token
+
+func NewClient(hostname string, port int, token string) *Client {
+	return &Client{
+		hostname:   hostname,
+		port:       port,
+		authToken:  token,
+		httpClient: &http.Client{},
+	}
+}
